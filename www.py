2cf7cb62891568613ef3a9a200a6abdd79fddaa7
thker_lee <thker_lee@163.com>
2015-07-04 17:07:15 +0800

    20150703 office

--- www.py
+++ www.py
@@ -13,21 +13,24 @@ class mainhandler(tornado.web.RequestHandler):
 
 	def get(self):
 		printlist=[
-		("shunfeng","shunfeng","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
-		,("shunfeng","print_demo","shunfeng.jpg")
+		("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
+		,("shunfeng","detail","shunfeng.jpg")
 		
 		]
 		self.render("index.html",list=printlist)
-class print_demo(tornado.web.RequestHandler):
-	
 
+class print_demo_frame(tornado.web.RequestHandler):
+	def get(self):
+		self.render("print_demo_frame.html")
+
+class print_demo(tornado.web.RequestHandler):
 
 	def get(self,danjumingchen):
 		
@@ -35,7 +38,7 @@ class print_demo(tornado.web.RequestHandler):
 		print_mingchen=print_mingchen[:-5];
 		print_mingchen=('').join(list(print_mingchen));
 		self.render("print_demo.html",mingchen=print_mingchen)
-	def post(self):
+	def post(self,danjumingchen):
 		
 		now=datetime.now()
 		now=now.strftime("%Y%m%d%H%M%S")
@@ -44,20 +47,33 @@ class print_demo(tornado.web.RequestHandler):
 		file_metas=self.request.files['printcvsfile']
 		for meta in file_metas:
 			filename=meta['filename']
-			filename=filename + now
-			#/////////////////
-			content=meta['body']
-			# save as a file
-			# filepath=os.path.join(upload_path,filename)
-			# with open(filepath,'w') as up:
-			# 	up.write(meta['body'])
+			filename=now +'.'+filename 
+			#/////////////////			
+			#save as a file
+			filepath=os.path.join(upload_path,filename)
+			with open(filepath,'w') as up:
+				up.write(meta['body'])
 			#/////////
-			reader=csv.reader(content)
+			datafile=open(filepath,'r')
+			# head=datafile.readline()
+			reader=csv.reader(datafile, delimiter=' ')	
+			# line=('').join(list(head))
+			# self.write(line)
+			head=reader.next()
+			detaildata=list()
+			sourcedata=list()
+			for   line in reader:
+				sourcedata.append(line)
+			for line in sourcedata:
+				nextline=zip(head,line)
+				detaildata.append(nextline)
+				pass
+			self.render("print_printdetal.html",printdata=sourcedata,mingchen=danjumingchen)
+			# for line in reader:
+			# 	line=('').join(list(line))
+			# 	self.write(line)
+			#  	self.write('</br>')
 
-			for line in reader:
-				for word in line:					
-					self.write(word)
-				self.write('</br>')
 		
 
 class upload(tornado.web.RequestHandler):
@@ -68,8 +84,11 @@ def main():
 	app=tornado.web.Application(
 		[
 		(r"/",mainhandler),
+		(r"/detail.html",print_demo_frame),
 		(r"/list/upload",print_demo),
 	 	(r"/list/(.*)",print_demo),
+	 	
+
 		
 		# (r"/templates/left-nav.html",left_nav),
 		# (r"/templates/right-nav.html",right_nav),
